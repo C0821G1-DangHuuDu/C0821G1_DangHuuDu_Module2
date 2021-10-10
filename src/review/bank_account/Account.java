d0b5980f0f4dd9ce252e53e5418b4bdf3c85da5c
C0821G1-DangHuuDu <huud90@gmail.com>
2021-10-10 17:20:44 +0700

    bai tap cuoi tuan

---
+++ src/review/bank_account/Account.java
@@ -0,0 +1,70 @@
+package review.bank_account;
+
+public class Account {
+    private long accountNumber;
+    private String name;
+    private double amount;
+    private final double interestRate=0.035;
+
+    public Account(){
+
+    }
+
+    public Account(long accountNumber, String name, double amount) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public Account(long accountNumber, String name) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+    }
+
+    public long getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getInterestRate() {
+        return this.interestRate;
+    }
+    public void addAmount(double money){
+        if (money<0){
+            System.out.println("Số tiền nạp vào tài khoản phải lớn hơn 0");
+        }else {
+            this.setAmount(this.getAmount()+money);
+        }
+
+    }
+    public void withdraw(double money){
+        if (money > this.getAmount()){
+            System.out.println("Số tiền rút không được lớn hơn số tiền trong tài khoản");
+        } else {
+            this.setAmount(this.getAmount()-money);
+        }
+    }
+    public void expire(){
+        this.setAmount(this.getAmount()+this.getAmount()*this.getInterestRate());
+    }
+
+}
