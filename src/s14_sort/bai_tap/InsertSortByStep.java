452bd7bffb08898065481de171e98d36d32f8823
C0821G1-DangHuuDu <huud90@gmail.com>
2021-10-24 20:43:31 +0700

    s14 sort

---
+++ src/s14_sort/bai_tap/InsertSortByStep.java
@@ -0,0 +1,60 @@
+package s14_sort.bai_tap;
+
+public class InsertSortByStep {
+    public void insertionSort(int arr[]) {
+        int valueToInsert;
+        int holePosition;
+
+
+         for (int i = 1; i < arr.length; i++) {
+
+            // chon mot gia tri de chen
+            valueToInsert = arr[i];
+
+            // lua chon vi tri de chen
+            holePosition = i;
+
+            // kiem tra xem so lien truoc co lon hon gia tri duoc chen khong
+            while (holePosition > 0 && arr[holePosition - 1] > valueToInsert) {
+                arr[holePosition] = arr[holePosition - 1];
+                holePosition--;
+                System.out.println("Di chuyen phan tu: " + arr[holePosition]);
+            }
+
+            if (holePosition != i) {
+                System.out.println(" Chen phan tu: " + valueToInsert
+                        + ", tai vi tri: " + holePosition);
+                // chen phan tu tai vi tri chen
+                arr[holePosition] = valueToInsert;
+            }
+
+            System.out.println("Vong lap thu " + i);
+            display(arr);
+        }
+    }
+
+    public void display(int arr[]) {
+        int i;
+        System.out.print("[");
+
+        // Duyet qua tat ca phan tu
+        for (i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.print("]\n");
+    }
+    public static void main(String[] args) {
+        // khoi tao mang arr
+        int arr[] = { 6, 7, 0, 2, 8, 1, 3, 9, 4, 5 };
+
+        InsertSortByStep sapXepChen = new InsertSortByStep();
+        System.out.println("Mang du lieu dau vao: ");
+        sapXepChen.display(arr);
+        System.out.println("-----------------------------");
+        sapXepChen.insertionSort(arr);
+        System.out.println("-----------------------------");
+        System.out.println("\nMang sau khi da sap xep: ");
+        sapXepChen.display(arr);
+    }
+}
