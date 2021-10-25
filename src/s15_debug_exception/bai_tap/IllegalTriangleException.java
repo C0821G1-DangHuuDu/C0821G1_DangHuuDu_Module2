6eb1418ec780ea4a38fb59f849714c832bdfd282
C0821G1-DangHuuDu <huud90@gmail.com>
2021-10-25 18:02:53 +0700

    s15 exception

---
+++ src/s15_debug_exception/bai_tap/IllegalTriangleException.java
@@ -0,0 +1,4 @@
+package s15_debug_exception.bai_tap;
+
+public class IllegalTriangleException {
+}
---
+++ src/s15_debug_exception/bai_tap/Triangle.java
@@ -0,0 +1,17 @@
+package s15_debug_exception.bai_tap;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhập vào độ dài cạnh 1: ");
+        int triangleEdge1=Integer.parseInt(input.nextLine());
+        System.out.print("Nhập vào độ dài cạnh 2: ");
+        int triangleEdge2=Integer.parseInt(input.nextLine());
+        System.out.print("Nhập vào độ dài cạnh 3: ");
+        int triangleEdge3=Integer.parseInt(input.nextLine());
+
+    }
+}
